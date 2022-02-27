304bfa30f8c4395324ba2ec4378862ae624e30a9
CPlaiz <treesarne@gmail.com>
2022-02-28 00:53:28 +0100

    Made abstract

--- src/main/java/de/silencio/activecraftcore/events/ActiveCraftEvent.java
+++ src/main/java/de/silencio/activecraftcore/events/ActiveCraftEvent.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class ActiveCraftEvent extends Event {
+public abstract class ActiveCraftEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
