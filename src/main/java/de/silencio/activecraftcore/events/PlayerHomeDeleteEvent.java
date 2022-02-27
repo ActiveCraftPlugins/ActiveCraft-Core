5fa20546fc100e19747da39104a69e17e252ef4f
CPlaiz <treesarne@gmail.com>
2022-02-27 23:28:24 +0100

    Home events

---
+++ src/main/java/de/silencio/activecraftcore/events/PlayerHomeDeleteEvent.java
@@ -0,0 +1,13 @@
+package de.silencio.activecraftcore.events;
+
+import de.silencio.activecraftcore.playermanagement.Profile;
+import lombok.*;
+import org.bukkit.Location;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class PlayerHomeDeleteEvent extends ActiveCraftEvent {
+    private final Profile profile;
+    private final Location location;
+    private boolean cancelled;
+}
