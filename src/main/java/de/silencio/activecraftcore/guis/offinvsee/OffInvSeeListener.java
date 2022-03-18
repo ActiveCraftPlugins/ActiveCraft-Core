aa64303b405fc2b349b71fb8f50f97dfea34de77
CPlaiz <treesarne@gmail.com>
2022-03-18 02:51:58 +0100

    Finally implemented OffInvSee

---
+++ src/main/java/de/silencio/activecraftcore/guis/offinvsee/OffInvSeeListener.java
@@ -0,0 +1,26 @@
+package de.silencio.activecraftcore.guis.offinvsee;
+
+import de.silencio.activecraftcore.guicreator.Gui;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+
+public class OffInvSeeListener implements Listener {
+
+
+    @EventHandler
+    public void onInvClick(InventoryCloseEvent event) {
+        Inventory inventory = event.getInventory();
+        Gui gui = Gui.ofInventory(inventory);
+        if (gui == null) return;
+        if (!(gui.getAssociatedGuiCreator() instanceof OffInvSeeGui offInvSeeGui)) return;
+        Player player = offInvSeeGui.getPlayer();
+        player.getInventory().setHelmet(inventory.getItem(11));
+        player.getInventory().setChestplate(inventory.getItem(12));
+        player.getInventory().setLeggings(inventory.getItem(13));
+        player.getInventory().setBoots(inventory.getItem(14));
+        player.getInventory().setItemInOffHand(inventory.getItem(16));
+    }
+}
