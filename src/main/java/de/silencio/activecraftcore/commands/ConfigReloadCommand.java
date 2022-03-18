d3e2acf03f99a281e8333001b9c531199ad38e79
CPlaiz <treesarne@gmail.com>
2022-03-18 02:42:17 +0100

    Added message and reloading of messages.yml

--- src/main/java/de/silencio/activecraftcore/commands/ConfigReloadCommand.java
+++ src/main/java/de/silencio/activecraftcore/commands/ConfigReloadCommand.java
@@ -1,6 +1,7 @@
 package de.silencio.activecraftcore.commands;
 
 import de.silencio.activecraftcore.exceptions.ActiveCraftException;
+import de.silencio.activecraftcore.messages.CommandMessages;
 import de.silencio.activecraftcore.utils.config.ConfigManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -17,6 +18,8 @@ public class ConfigReloadCommand extends ActiveCraftCommand {
     public void runCommand(CommandSender sender, Command command, String label, String[] args) throws ActiveCraftException {
         checkPermission(sender, "acreload");
         ConfigManager.reloadAll();
+        ConfigManager.loadMessageConfig();
+        sendMessage(sender, CommandMessages.AC_RELOADED());
     }
 
     @Override
