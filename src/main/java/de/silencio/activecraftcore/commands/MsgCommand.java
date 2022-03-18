b722693c007bc91c5ba3be290bf7378940458606
CPlaiz <treesarne@gmail.com>
2022-03-18 02:45:58 +0100

    Partly moved code to manager class

--- src/main/java/de/silencio/activecraftcore/commands/MsgCommand.java
+++ src/main/java/de/silencio/activecraftcore/commands/MsgCommand.java
@@ -1,22 +1,14 @@
 package de.silencio.activecraftcore.commands;
 
-import de.silencio.activecraftcore.ActiveCraftCore;
-import de.silencio.activecraftcore.events.MsgEvent;
 import de.silencio.activecraftcore.exceptions.ActiveCraftException;
-import de.silencio.activecraftcore.exceptions.InvalidPlayerException;
+import de.silencio.activecraftcore.manager.MessageManager;
 import de.silencio.activecraftcore.messages.CommandMessages;
-import de.silencio.activecraftcore.playermanagement.PlayerQueue;
 import de.silencio.activecraftcore.playermanagement.Profile;
 import de.silencio.activecraftcore.utils.ColorUtils;
 import de.silencio.activecraftcore.utils.ComparisonType;
-import de.silencio.activecraftcore.utils.config.ConfigManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MsgCommand extends ActiveCraftCommand {
@@ -33,41 +25,9 @@ public class MsgCommand extends ActiveCraftCommand {
         checkTargetSelf(sender, profile.getName());
 
         String message = combineArray(args, 1);
-        message = ColorUtils.replaceColor(message);
-        message = ColorUtils.replaceFormat(message);
-        String finalMessage = message;
-        if (sender instanceof Player) {
-            sendMessage(sender, CommandMessages.MSG_PREFIX_TO(profile, message));
-            PlayerQueue.add(profile, () -> {
-                Player target = Bukkit.getPlayer(profile.getName());
-                MsgEvent event = new MsgEvent(sender, profile, finalMessage);
-                Bukkit.getPluginManager().callEvent(event);
-                if (event.isCancelled()) return;
-                sendMessage(target, CommandMessages.MSG_PREFIX_FROM(sender, event.getMessage()));
-                target.playSound(target.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1f, 1f);
-                try {
-                    ActiveCraftCore.getMsgPlayerStoring().put(target, getProfile(sender.getName()));
-                } catch (InvalidPlayerException ignored) {
-                }
-                for(Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                    if (!onlinePlayer.hasPermission("activecraft.msg.spy")) continue;
-                    if(!getProfile(onlinePlayer).canReceiveSocialspy()) continue;
-                    if (onlinePlayer != sender && onlinePlayer != target)
-                        sendMessage(onlinePlayer, CommandMessages.SOCIALSPY_PREFIX_TO(target, sender, finalMessage));
-                }
-                if (ConfigManager.getMainConfig().isSocialSpyToConsole())
-                    sendMessage(Bukkit.getConsoleSender(), CommandMessages.SOCIALSPY_PREFIX_TO(target, sender, event.getMessage()));
-            });
-        } else {
-            PlayerQueue.add(profile, () -> {
-                Player target = Bukkit.getPlayer(profile.getName());
-                MsgEvent event = new MsgEvent(sender, profile, finalMessage);
-                Bukkit.getPluginManager().callEvent(event);
-                if (event.isCancelled()) return;
-                sendMessage(target, CommandMessages.CONSOLE_MSG_PREFIX(event.getMessage()));
-                target.playSound(target.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1f, 1f);
-            });
-        }
+        message = ColorUtils.replaceColorAndFormat(message);
+        sendMessage(sender, CommandMessages.MSG_PREFIX_TO(profile, message));
+        MessageManager.sendDM(profile, sender, message);
     }
 
     @Override
--- src/main/java/de/silencio/activecraftcore/commands/ReplyCommand.java
+++ src/main/java/de/silencio/activecraftcore/commands/ReplyCommand.java
@@ -1,17 +1,9 @@
 package de.silencio.activecraftcore.commands;
 
-import de.silencio.activecraftcore.ActiveCraftCore;
-import de.silencio.activecraftcore.events.MsgEvent;
 import de.silencio.activecraftcore.exceptions.ActiveCraftException;
-import de.silencio.activecraftcore.exceptions.InvalidPlayerException;
-import de.silencio.activecraftcore.messages.CommandMessages;
-import de.silencio.activecraftcore.playermanagement.PlayerQueue;
-import de.silencio.activecraftcore.playermanagement.Profile;
+import de.silencio.activecraftcore.manager.MessageManager;
 import de.silencio.activecraftcore.utils.ColorUtils;
 import de.silencio.activecraftcore.utils.ComparisonType;
-import de.silencio.activecraftcore.utils.config.ConfigManager;
-import org.bukkit.Bukkit;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -28,35 +20,9 @@ public class ReplyCommand extends ActiveCraftCommand {
     public void runCommand(CommandSender sender, Command command, String label, String[] args) throws ActiveCraftException {
         Player player = getPlayer(sender);
         checkPermission(sender, "reply");
-        Profile profile = ActiveCraftCore.getMsgPlayerStoring().get(player);
-
-        if (profile == null) throw new InvalidPlayerException(null);
-
         checkArgsLength(args, ComparisonType.GREATER, 0);
 
-        String message = combineArray(args);
-        message = ColorUtils.replaceColor(message);
-        message = ColorUtils.replaceFormat(message);
-        String finalMessage = message;
-        sendMessage(player, CommandMessages.MSG_PREFIX_TO(profile, message));
-        PlayerQueue.add(profile, () -> {
-            Player target = Bukkit.getPlayer(profile.getName());
-            MsgEvent event = new MsgEvent(sender, profile, finalMessage);
-            Bukkit.getPluginManager().callEvent(event);
-            if (event.isCancelled()) return;
-            sendMessage(target, CommandMessages.MSG_PREFIX_FROM(sender, event.getMessage()));
-            target.playSound(target.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1f, 1f);
-
-            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                if (!onlinePlayer.hasPermission("activecraft.msg.spy")) continue;
-                if (!getProfile(onlinePlayer).canReceiveSocialspy()) continue;
-                if (onlinePlayer != sender && onlinePlayer != target)
-                    sendMessage(onlinePlayer, CommandMessages.SOCIALSPY_PREFIX_TO(target, sender, finalMessage));
-            }
-            if (ConfigManager.getMainConfig().isSocialSpyToConsole())
-                sendMessage(Bukkit.getConsoleSender(), CommandMessages.SOCIALSPY_PREFIX_TO(target, sender, event.getMessage()));
-        });
-
+        MessageManager.reply(sender, ColorUtils.replaceColorAndFormat(combineArray(args)));
     }
 
     @Override
