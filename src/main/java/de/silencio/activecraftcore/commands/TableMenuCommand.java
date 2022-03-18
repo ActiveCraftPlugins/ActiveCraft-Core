3debe4c83ab58a2025e56de80b5d3854fdee9d0d
CPlaiz <treesarne@gmail.com>
2022-03-18 03:04:29 +0100

    Reworked TableMenu with new GuiCreator

--- src/main/java/de/silencio/activecraftcore/commands/TableMenuCommand.java
+++ src/main/java/de/silencio/activecraftcore/commands/TableMenuCommand.java
@@ -1,9 +1,9 @@
 package de.silencio.activecraftcore.commands;
 
 import de.silencio.activecraftcore.exceptions.ActiveCraftException;
-import de.silencio.activecraftcore.guicreator.GuiCloseItem;
 import de.silencio.activecraftcore.guicreator.GuiCreator;
 import de.silencio.activecraftcore.guicreator.GuiItem;
+import de.silencio.activecraftcore.guicreator.GuiNavigator;
 import de.silencio.activecraftcore.messages.TableMenuMessages;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -18,6 +18,8 @@ public class TableMenuCommand extends ActiveCraftCommand {
         super("tablemenu");
     }
 
+
+
     @Override
     public void runCommand(CommandSender sender, Command command, String label, String[] args) throws ActiveCraftException {
         checkPermission(sender, "tablemenu");
@@ -25,21 +27,32 @@ public class TableMenuCommand extends ActiveCraftCommand {
         GuiCreator guiCreator = new GuiCreator("table_menu", 6, TableMenuMessages.TABLEMENU_TITLE()) {
             @Override
             public void refresh() {
-                GuiCloseItem guiCloseItem = new GuiCloseItem(49);
-                setItemInSlot(new GuiItem(Material.CRAFTING_TABLE).setDisplayName(TableMenuMessages.TABLEMENU_CRAFTING_TABLE()).setMovable(false).setClickSound(true), 13);
-                setItemInSlot(new GuiItem(Material.CARTOGRAPHY_TABLE).setDisplayName(TableMenuMessages.TABLEMENU_CARTOGRAPHY_TABLE()).setMovable(false).setClickSound(true), 20);
-                setItemInSlot(new GuiItem(Material.STONECUTTER).setDisplayName(TableMenuMessages.TABLEMENU_STONECUTTER()).setMovable(false).setClickSound(true), 21);
-                setItemInSlot(new GuiItem(Material.ANVIL).setDisplayName(TableMenuMessages.TABLEMENU_ANVIL()).setMovable(false).setClickSound(true), 22);
-                setItemInSlot(new GuiItem(Material.GRINDSTONE).setDisplayName(TableMenuMessages.TABLEMENU_GRINDSTONE()).setMovable(false).setClickSound(true), 23);
-                setItemInSlot(new GuiItem(Material.LOOM).setDisplayName(TableMenuMessages.TABLEMENU_LOOM()).setMovable(false).setClickSound(true), 24);
-                setItemInSlot(new GuiItem(Material.SMITHING_TABLE).setDisplayName(TableMenuMessages.TABLEMENU_SMITHING_TABLE()).setMovable(false).setClickSound(true), 31);
-                guiCloseItem.setClickSound(true);
-                guiCloseItem.setMovable(false);
+                setItem(new GuiItem(Material.CRAFTING_TABLE)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_CRAFTING_TABLE())
+                        .addClickListener(guiClickEvent -> player.performCommand("craftingtable")), 13);
+                setItem(new GuiItem(Material.CARTOGRAPHY_TABLE)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_CARTOGRAPHY_TABLE())
+                        .addClickListener(guiClickEvent -> player.performCommand("cartographytable")), 20);
+                setItem(new GuiItem(Material.STONECUTTER)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_STONECUTTER())
+                        .addClickListener(guiClickEvent -> player.performCommand("stonecutter")), 21);
+                setItem(new GuiItem(Material.ANVIL)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_ANVIL())
+                        .addClickListener(guiClickEvent -> player.performCommand("anvil")), 22);
+                setItem(new GuiItem(Material.GRINDSTONE)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_GRINDSTONE())
+                        .addClickListener(guiClickEvent -> player.performCommand("grindstone")), 23);
+                setItem(new GuiItem(Material.LOOM)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_LOOM())
+                        .addClickListener(guiClickEvent -> player.performCommand("loom")), 24);
+                setItem(new GuiItem(Material.SMITHING_TABLE)
+                        .setDisplayName(TableMenuMessages.TABLEMENU_SMITHING_TABLE())
+                        .addClickListener(guiClickEvent -> player.performCommand("smithingtable")), 31);
                 fillBackground(true);
-                setCloseItem(guiCloseItem);
+                setCloseItem(49);
             }
         };
-        player.openInventory(guiCreator.build().getInventory());
+        GuiNavigator.push(player, guiCreator.build());
     }
 
     @Override
