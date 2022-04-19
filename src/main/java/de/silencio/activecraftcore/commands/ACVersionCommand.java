0bad62113d90c3aaf1e2cb40eab098e6f6b278c9
CPlaiz <treesarne@gmail.com>
2022-04-19 11:23:33 +0200

    refactor: change exception handling

--- src/main/java/de/silencio/activecraftcore/commands/ACVersionCommand.java
+++ src/main/java/de/silencio/activecraftcore/commands/ACVersionCommand.java
@@ -30,32 +30,29 @@ public class ACVersionCommand extends ActiveCraftCommand {
     public void runCommand(CommandSender sender, Command command, String label, String[] args) throws ActiveCraftException {
         checkPermission(sender, "version");
         checkArgsLength(args, ComparisonType.EQUAL, 0);
-        try {
-            HashMap<String, Integer> plugins = WebReader.getACVersionMap();
-            ArrayList<String> keys = new ArrayList<>(plugins.keySet());
-            Collections.sort(keys);
-            for (String key : keys) {
-                Plugin plugin = Bukkit.getPluginManager().getPlugin(key);
-                if (plugin == null) {
-                    sendMessage(sender, ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Not installed.");
-                    continue;
-                }
-                new UpdateChecker(plugin, plugins.get(key)).getVersion(version -> {
-                    if (!plugin.getDescription().getVersion().equals(version)) {
-                        ComponentBuilder builder = new ComponentBuilder();
-                        builder.append(new TextComponent(ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.RED + "Update Available. "));
-                        builder.append(new TextComponent(ChatColor.RED + "\nCurrent: " + ChatColor.DARK_RED + plugin.getDescription().getVersion() + ChatColor.RED + " Newest: " + ChatColor.DARK_RED + version + ChatColor.RED + "."));
-                        TextComponent linkComponent = new TextComponent();
-                        linkComponent.setText(ChatColor.AQUA + " [Link]");
-                        linkComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("Click to open link")));
-                        linkComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/resources/" + plugin.getName().toLowerCase() + "." + plugins.get(key)));
-                        builder.append(linkComponent);
-                        sendMessage(sender, builder.create());
-                    } else
-                        sendMessage(sender, ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.GREEN + "Latest.");
-                });
+        HashMap<String, Integer> plugins = WebReader.getACVersionMap();
+        ArrayList<String> keys = new ArrayList<>(plugins.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            Plugin plugin = Bukkit.getPluginManager().getPlugin(key);
+            if (plugin == null) {
+                sendMessage(sender, ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.GRAY + "Not installed.");
+                continue;
             }
-        } catch (IOException ignored) {
+            new UpdateChecker(plugin, plugins.get(key)).getVersion(version -> {
+                if (!plugin.getDescription().getVersion().equals(version)) {
+                    ComponentBuilder builder = new ComponentBuilder();
+                    builder.append(new TextComponent(ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.RED + "Update Available. "));
+                    builder.append(new TextComponent(ChatColor.RED + "\nCurrent: " + ChatColor.DARK_RED + plugin.getDescription().getVersion() + ChatColor.RED + " Newest: " + ChatColor.DARK_RED + version + ChatColor.RED + "."));
+                    TextComponent linkComponent = new TextComponent();
+                    linkComponent.setText(ChatColor.AQUA + " [Link]");
+                    linkComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("Click to open link")));
+                    linkComponent.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.spigotmc.org/resources/" + plugin.getName().toLowerCase() + "." + plugins.get(key)));
+                    builder.append(linkComponent);
+                    sendMessage(sender, builder.create());
+                } else
+                    sendMessage(sender, ChatColor.GOLD + key + ChatColor.DARK_AQUA + " - " + ChatColor.GREEN + "Latest.");
+            });
         }
     }
 
